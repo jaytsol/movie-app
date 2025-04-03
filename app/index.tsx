338965b28ea57a4cbba81c144c6858d27258edcb
jaytsol <jaytsol@naver.com>
2025-04-04 02:00:01 +0900

    Added nativewind setup

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     // <View className="flex-1 justify-center items-center">
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500 font-bold">Welcome!</Text>
+      <Text className="text-5xl text-light-100 font-bold">Welcome!</Text>
     </View>
   );
 }
