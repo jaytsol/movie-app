d3019b20cd53e8db82b0d11db6826e95fa8fefce
jaytsol <jaytsol@naver.com>
2025-04-19 01:10:21 +0900

    Hide StatusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from "expo-router";
 import "./global.css";
-import React from 'react'
+import React from "react";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
